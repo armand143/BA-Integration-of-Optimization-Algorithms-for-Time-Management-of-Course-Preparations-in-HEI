845fc72e75a70cbb020c3d0735f3eab0c745f39d
armand143 <armandborel@yahoo.fr>
2023-10-13 17:01:15 +0200

    migrations applied

--- webApp_django/timeoptApp/settings.py
+++ webApp_django/timeoptApp/settings.py
@@ -96,12 +96,12 @@ WSGI_APPLICATION = 'timeoptApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # env = environ.Env()
@@ -110,9 +110,9 @@ DATABASES = {
 #     'default': env.db('postgresql://postgres:pSgTC4XdL1BfesME9IMk@containers-us-west-125.railway.app:6263/railway')
 # }
 
-# DATABASES = {
-#     'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
-# }
+DATABASES = {
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+}
 
 
 # Password validation
